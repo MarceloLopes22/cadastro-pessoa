e7e75acdc2f723447e83a6aae048a868b0057eaa
MarceloLopes22 <marcelomlopes2@gmail.com>
2020-04-07 23:00:41 -0300

    removendo s da constante

--- cadastro-pessoa-angular/src/app/servicos/pessoa.api.ts
+++ cadastro-pessoa-angular/src/app/servicos/pessoa.api.ts
@@ -1,2 +1,2 @@
-export const PESSOA_API: string = "https://localhost:8080";
+export const PESSOA_API: string = "http://localhost:8080";
 //export const PESSOA_API: string = "https://api-rest-cadastro-pessoa.herokuapp.com";
